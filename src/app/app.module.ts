f5e0faf257ea063c012334340853a694dd28faa9
Sanjay Kumawat <sanjaykumawat134@gmail.com>
2021-05-29 15:03:19 +0530

    Shopping-list component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import {HeaderComponent} from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 import { RecipeItemComponent } from './recipes/recipes-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipeItemComponent
+    AppComponent, HeaderComponent, RecipesComponent, RecipesListComponent, RecipeItemComponent, ShoppingListComponent, ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
