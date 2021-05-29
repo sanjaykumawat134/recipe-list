e3d92d94f2f17c6c0828ab3496b41dd0731c6fd2
Sanjay Kumawat <sanjaykumawat134@gmail.com>
2021-05-29 14:34:13 +0530

    main recipe component created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent
+    AppComponent, HeaderComponent, RecipesComponent
   ],
   imports: [
     BrowserModule,
