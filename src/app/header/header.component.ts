7158057b7ef7542f44807c0a65ff25c87c42e937
Sanjay Kumawat <sanjaykumawat134@gmail.com>
2021-05-29 14:08:42 +0530

    created header component.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:'header-app',
+  templateUrl:'header.component.html',
+  styleUrls:[
+    "header.component.css"
+  ]
+})
+export class HeaderComponent{
+   title ="Header Component Created";
+}
